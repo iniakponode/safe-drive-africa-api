56cf9f07ac5b1edb6f235f3b67d54f89ab603078
Iniks <iniakpothompson.coins@gmail.com>
2023-08-23 18:17:11 +0100

    Add installled mysqlclient

--- app/api/database/base.py
+++ app/api/database/base.py
@@ -21,7 +21,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
